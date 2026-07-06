3f7fcb2ba243e19c170e058c66dfe709f12b7a40
ztzz <ztzz.xyz@gmail.com>
2026-07-06 23:15:34 +0800

    paidUpto 2099

--- gen_license.py
+++ gen_license.py
@@ -25,7 +25,7 @@ def gen_license(id, hardwareId):
     private_key_content = get_private_key()
     private_key = RSA.import_key(private_key_content)
 
-    licensePart = f'{{"licenseeName": "{id}", "hardwareID": "{hardwareId}", "paidUpTo": "2025-08-23"}}'
+    licensePart = f'{{"licenseeName": "{id}", "hardwareID": "{hardwareId}", "paidUpTo": "2099-01-01"}}'
     digest = SHA256.new(licensePart.encode('utf-8'))
     signature = pkcs1_15.new(private_key).sign(digest)
 
@@ -45,21 +45,33 @@ def gen_license(id, hardwareId):
     with SQLiteDB('gitee.db') as db:
         db.insert_or_update("USERS", {'USERID': id, 'HARDWAREID': hardwareId}, 'USERID')
 
+def refresh_all_licenses():
+    with SQLiteDB('gitee.db') as db:
+        all_users = db.fetch_all("SELECT * FROM USERS")
+        print(all_users)
+        for user in all_users:
+            print("开始创建key")
+            gen_license(user['USERID'], user['HARDWAREID'])
+            print(f"[{user['USERID']}] {user['HARDWAREID']}")
+            print(f"生成key成功")
 
 if __name__ == "__main__":
-    # 创建实例并获取数据
-    fetcher = GiteeIssueCommentFetcher()
-
-    try:
-        # 获取评论
-        comment = fetcher.get_comments_if_text_in_comments(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-        if not comment:
+    if sys.argv[1] == 'all':
+        refresh_all_licenses()
+    else:
+        # 创建实例并获取数据
+        fetcher = GiteeIssueCommentFetcher()
+    
+        try:
+            # 获取评论
+            comment = fetcher.get_comments_if_text_in_comments(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+            if not comment:
+                sys.exit(1)
+            print("开始创建key")
+            gen_license(comment['user']['id'], comment['body'])
+            print(f"[{comment['user']['name']}] {comment['body']}")
+            print(f"评论创建时间: {comment['created_at']}")
+            print(f"生成key成功")
+        except Exception as e:
+            print(f"发生错误: {e}")
             sys.exit(1)
-        print("开始创建key")
-        gen_license(comment['user']['id'], comment['body'])
-        print(f"[{comment['user']['name']}] {comment['body']}")
-        print(f"评论创建时间: {comment['created_at']}")
-        print(f"生成key成功")
-    except Exception as e:
-        print(f"发生错误: {e}")
-        sys.exit(1)
